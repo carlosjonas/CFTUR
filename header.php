7fc1fe79762ce7fa3448399f59fda04e3f748237
Carlos Jonas <jonas.fonseca@opovodigital.com>
2021-09-14 22:31:59 -0300

    começando o header , adicionando alguns links meta e começando o carrossel

---
+++ header.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CFTUR</title>
+
+	<!--TAGS META-->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="keywords" content="Viagens,Agencia de viagens,CFTUR,Viagens em fortaleza,Fortaleza"/>
+	<meta name="description" content="Pagina de agencia de viagens cftur"/>
+	<meta name="author" content="Carlos Jonas"/>
+
+	<!--FAVICON-->
+	<link rel="sortcut icon" href="#" type="image/ico" />
+
+	<!--CSS-->
+	<link rel="stylesheet" type="text/css" href="public/css/style.css">
+
+	<!--BOOTSTRAP-->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	
+</head>
+<body>
+	<header>
+		<div class="container-fluid">
+			<div id="carrosel" class="carousel slide" data-ride="carousel">
+			  	<div class="carousel-inner">
+			    	<div class="carousel-item active">
+			      		<img class="d-block w-100" src="public/img/logo4.jpg" alt="Primeiro Slide">
+			      		<div class="carousel-caption d-none d-md-block">
+						    <h5>CFTUR...</h5>
+						</div>
+			   		</div>
+			    	<div class="carousel-item">
+			      		<img class="d-block w-100" src="public/img/logo4.jpg" alt="Segundo Slide">
+			      		<div class="carousel-caption d-none d-md-block">
+						    <h5>Levando seus sonhos...</h5>
+						</div>
+			    	</div>
+				    <div class="carousel-item">
+				      <img class="d-block w-100" src="public/img/logo4.jpg" alt="Terceiro Slide">
+				      <div class="carousel-caption d-none d-md-block">
+						    <h5>Sempre mais longe!</h5>
+						</div>
+				    </div>
+			  	</div>
+				<a class="carousel-control-prev" href="#carrosel" role="button" data-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="sr-only">Anterior</span>
+				</a>
+			  	<a class="carousel-control-next" href="#carrosel" role="button" data-slide="next">
+				    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+				    <span class="sr-only">Próximo</span>
+			  	</a>
+			</div>
+
+			<!--funciona-->
+			<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+			  <div class="carousel-inner">
+			    <div class="carousel-item active">
+			      <img class="d-block w-100" src="public/img/logo4.jpg" alt="Primeiro Slide">
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block w-100" src="public/img/logo.jpg" alt="Segundo Slide">
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block w-100" src=".../800x400?auto=yes&bg=555&fg=333&text=Terceiro Slide" alt="Terceiro Slide">
+			    </div>
+			  </div>
+			</div>
+		</div>
+
+
+	</header>
+	<footer>
+		<!--SCRIPTS-->
+		<script type="text/javascript" src="script.js"></script>
+	</footer>
+</body>
+</html>
