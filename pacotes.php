7d9df074a665afb082a267b7a6025df83612791d
Carlos Jonas <jonas.fonseca@opovodigital.com>
2021-09-16 21:46:01 -0300

    Adicionando o footer e a página home de pacotes

---
+++ pacotes.php
@@ -0,0 +1,23 @@
+<?php require_once 'header.php' ?>
+
+<div class="row">
+	<div class="container">
+		<div class="text-center">
+			<div class="card" style="width: 18rem;">
+  				<img src="..." class="card-img-top" alt="...">
+	  			<div class="card-body">
+	    		<h5 class="card-title">Rio de Janeiro</h5>
+	    		<p class="card-text">Vamos visitar uma das maiores cidades do nosso Brasil e desfrutar de aventuras inesquecíveis.</p>
+	    		<a href="#" class="btn btn-primary">Mais detalhes</a>
+	  			</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+
+
+
+<?php require_once 'footer.php' ?>
