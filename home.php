76adc2bfb3f591a4d38afd4652048f9d920eace9
Carlos Jonas <jonas.fonseca@opovodigital.com>
2023-01-16 22:24:48 -0300

    feature: chamando a função de para selecionar as viagens

--- home.php
+++ home.php
@@ -2,11 +2,11 @@
 
  require __DIR__.'/vendor/autoload.php';
 
+use \App\Entidade\Viagem;
 
-
+$listaViagens = Viagem::getViagens();
 
 include  __DIR__.'\includes\header.php';
 include  __DIR__.'\includes\listagem.php';
 include  __DIR__.'\includes\footer.php';
-
  ?>
