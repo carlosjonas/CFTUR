e5a0d1238d357d746d027caf168d2fd78e0a690f
Carlos Jonas <jonas.fonseca@opovodigital.com>
2023-01-17 21:39:43 -0300

    feature: adicionando a pasta vendor com dependencias de autoload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '7d9df074a665afb082a267b7a6025df83612791d',
+        'name' => 'cftur/viagens',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'cftur/viagens' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '7d9df074a665afb082a267b7a6025df83612791d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
