77faf8a0839eed86c8df758461a2d1779e63545b
Carlos Jonas <jonas.fonseca@opovodigital.com>
2023-01-17 23:10:57 -0300

    refactor: editando o card de listagem

--- includes/listagem.php
+++ includes/listagem.php
@@ -6,16 +6,15 @@
 			<div class="card mt-3">
 			  <img src="' .$viagem->imagem. '" class="card-img-top" alt="' .$viagem->imagem. '">
 			  <div class="card-body">
-			    <h5 class="card-title">' .$viagem->titulo. '</h5>
+			    <h3 class="card-title text-center">' .$viagem->titulo. '</h3>
 			    <p class="card-text">' .$viagem->descricao. '</p>
-			    <p class="card-text">Vagas: ' .$viagem->vagas. '</p>
-			    <p class="card-text">Valor: R$ ' .$viagem->valor. '</p>
+				<p class="card-text">Vagas: ' .$viagem->vagas. '</p>
+				<p class="card-text">Valor: R$ ' .$viagem->valor. '</p>				    
 			    <p class="card-text"><small class="text-muted">De: ' .date('d/m/Y',strtotime($viagem->data_inicio)). ' Até: ' .date('d/m/Y',strtotime($viagem->data_final)). '</small></p>
 			  </div>
 			  <div class="card-body">
 			    <p class="card-text">
-			    	<a href="editar.php?id=' .$viagem->id. '"><button type="button" class="btn btn-primary">Editar</button></a> <a href="excluir.php?id=' .$viagem->id. '"><button type="button" class="btn btn-danger">Excluir</button></a>
-					
+			    	<a href="editar.php?id=' .$viagem->id. '"><button type="button" class="btn btn-primary">Editar</button></a> <a href="excluir.php?id=' .$viagem->id. '"><button type="button" class="btn btn-danger">Excluir</button></a>		
 				</p>
 			  </div>
 			</div>';
