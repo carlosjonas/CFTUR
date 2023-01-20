c0c55fd76b4ea3d5bca2c506a4d374e092ef3c8b
Carlos Jonas <jonas.fonseca@opovodigital.com>
2023-01-19 23:13:41 -0300

    refactor: adicionando os valores nos campos do formulário

--- includes/formulario.php
+++ includes/formulario.php
@@ -3,32 +3,80 @@
 			<a class="btn btn-success mt-5" href="home.php">Voltar</a>
 		</section>
 
-		<h2 class="mt-3">Cadastrar Viagem</h2>
+		<h2 class="mt-3"><?= TITLE; ?></h2>
 
 		<form method="POST" class="mb-3">
 			<div class="form-group mt-3">
 				<label>Título</label>
-				<input type="text" class="form-control" name="titulo" required>
+				<input type="text" class="form-control" name="titulo" 
+					<?php 
+						if (@$viagem != 'null') {
+							echo 'value="'.@$viagem->titulo.'"';			
+						}else{
+							echo 'null';
+						} 
+					?> 
+				required>
 			</div>
 			<div class="form-group mt-3">
 				<label>Descrição</label>
-				<textarea class="form-control" name="descricao" rows="5"  required></textarea>
+				<textarea class="form-control" name="descricao" rows="5"  required>
+					<?php 
+						if (@$viagem != 'null') {
+							echo @$viagem->descricao;			
+						}else{
+							echo 'null';
+						} 
+					?>
+				</textarea>
 			</div>
 			<div class="form-group mt-3">
 				<label>Data de Início</label>
-				<input type="date" class="form-control" name="data_inicio"  required>
+				<input type="date" class="form-control" name="data_inicio" 
+					<?php 
+						if (@$viagem != 'null') {
+							echo 'value="'.@$viagem->data_inicio.'"';			
+						}else{
+							echo 'null';
+						} 
+					?>  
+				required>
 			</div>
 			<div class="form-group mt-3">
 				<label>Data de Finalização</label>
-				<input type="date" class="form-control" name="data_final"  required>
+				<input type="date" class="form-control" name="data_final" 
+					<?php 
+						if (@$viagem != 'null') {
+							echo 'value="'.@$viagem->data_final.'"';			
+						}else{
+							echo 'null';
+						} 
+					?>
+				required>
 			</div>
 			<div class="form-group mt-3">
 				<label>Valor</label>
-				<input type="number" class="form-control" name="valor" required> 
+				<input type="number" class="form-control" name="valor" 
+					<?php 
+						if (@$viagem != 'null') {
+							echo 'value="'.@$viagem->valor.'"';			
+						}else{
+							echo 'null';
+						} 
+					?>
+				required> 
 			</div>
 			<div class="form-group mt-3">
 				<label>Número de vagas</label>
-				<input type="number" class="form-control" name="vagas" required> 
+				<input type="number" class="form-control" name="vagas" 
+					<?php 
+						if (@$viagem != 'null') {
+							echo 'value="'.@$viagem->vagas.'"';			
+						}else{
+							echo 'null';
+						} 
+					?> 
+				required> 
 			</div>
 			<div class="form-group mt-3">
 				<label>Status</label>
@@ -38,16 +86,35 @@
 							<input type="radio" name="ativo" value="1" checked> Ativo
 						</label>
 					</div>
+
 					<div class="form-check form-check-inline">
 						<label class="form-control">
-							<input type="radio" name="inativo" value="0"> Inativo
+							<input type="radio" name="ativo" value="0" 
+								<?php
+									if (@$viagem != 'null') {
+										if (@$viagem->ativo == '0') {
+											echo "checked";
+										}else{
+											echo "";
+										}
+									}
+								?>
+						> Inativo
 						</label>
 					</div>
 				</div>
 			</div>
 			<div class="form-group mt-3">
 				<label>Imagem</label>
-				<input type="file" class="form-control" name="imagem" required>
+				<input type="file" class="form-control" name="imagem" 
+					<?php 
+						if (@$viagem != 'null') {
+							echo 'value="'.@$viagem->imagem.'"';			
+						}else{
+							echo 'null';
+						} 
+					?>
+				required>
 			</div>
 			<div class="form-group mt-3">
 				<button type="submit" class="btn btn-success">Enviar</button>
