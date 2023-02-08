90fc9c4c4dc2d9a6b1f899bcef7633a4a6d88469
Carlos Jonas <jonas.fonseca@opovodigital.com>
2023-02-07 22:02:17 -0300

    refactor: modificando a estrutura do carrosel de jeri

--- includes/galeria_fotos.php
+++ includes/galeria_fotos.php
@@ -1,54 +1,28 @@
 	<main>
-		<section>
-			<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+			<section id="banner-rotativo" class="carousel slide" data-bs-ride="true">
 			  <div class="carousel-indicators">
-			    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-			    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-			    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-			    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-current="true" aria-label="Slide 1"></button>
-			    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 2"></button>
-			    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 3"></button>
-			    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6" aria-current="true" aria-label="Slide 1"></button>
-			    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="7" aria-label="Slide 2"></button>
-			    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="8" aria-label="Slide 3"></button>
+
+			  	<?php for ($i=0; $i < 6; $i++): ?>
+			  		<button type="button" data-bs-target="#banner-rotativo" data-bs-slide-to="<?= $i;?>" class="<?php if ($i == 0) { echo "active";	}else{ echo "";} ?>" aria-current="true" aria-label="Slide <?= $i+1;?>"></button>
+			  	<?php endfor ?>
+
 			  </div>
 			  <div class="carousel-inner">
-			    <div class="carousel-item active">
-			      <img src="public/img/jeri/j1.jpg" class="d-block w-100" alt="...">
-			    </div>
-			    <div class="carousel-item">
-			      <img src="public/img/jeri/j1.jpg" class="d-block w-100" alt="...">
-			    </div>
-			    <div class="carousel-item">
-			      <img src="public/img/jeri/j1.jpg" class="d-block w-100" alt="...">
-			    </div>
-			    <div class="carousel-item active">
-			      <img src="public/img/jeri/j1.jpg" class="d-block w-100" alt="...">
-			    </div>
-			    <div class="carousel-item">
-			      <img src="public/img/jeri/j1.jpg" class="d-block w-100" alt="...">
-			    </div>
-			    <div class="carousel-item">
-			      <img src="public/img/jeri/j1.jpg" class="d-block w-100" alt="...">
-			    </div>
-			    <div class="carousel-item active">
-			      <img src="public/img/jeri/j1.jpg" class="d-block w-100" alt="...">
-			    </div>
-			    <div class="carousel-item">
-			      <img src="public/img/jeri/j1.jpg" class="d-block w-100" alt="...">
-			    </div>
-			    <div class="carousel-item">
-			      <img src="public/img/jeri/j1.jpg" class="d-block w-100" alt="...">
-			    </div>
+
+			  	<?php for ($j=1; $j < 7; $j++): ?>
+			  		<figure class="carousel-item <?php if ($j == 1) { echo "active"; }else{ echo "";} ?>">
+				      <img src="public/img/jeri/j<?= $j ;?>.jpg" class="d-block w-100" alt="Imagem de Jericoacoara <?= $j ;?>">
+				    </figure>
+			  	<?php endfor ?>
+
 			  </div>
-			  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+			  <button class="carousel-control-prev" type="button" data-bs-target="#banner-rotativo" data-bs-slide="prev">
 			    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
 			    <span class="visually-hidden">Previous</span>
 			  </button>
-			  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+			  <button class="carousel-control-next" type="button" data-bs-target="#banner-rotativo" data-bs-slide="next">
 			    <span class="carousel-control-next-icon" aria-hidden="true"></span>
 			    <span class="visually-hidden">Next</span>
 			  </button>
-			</div>
-		</section>
+			</section>
 	</main>
